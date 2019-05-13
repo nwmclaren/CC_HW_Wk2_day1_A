d2f37de607301440564d4573c6934f8923619c18
nwmclaren <nwmclaren@aol.com>
2019-05-13 18:40:22 +0100

    CC_HW_Wk2_Day1_all

---
+++ student.rb
@@ -0,0 +1,31 @@
+class Student
+
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def name
+    return @name
+  end
+
+  def cohort
+    return @cohort
+  end
+
+  def set_name(name)
+    @name = name
+  end
+
+  def set_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def student_talk
+    return "I can talk"
+  end
+
+  def student_language(language)
+    return "I love #{language}"
+  end
+end
